4fed964f5ed9ca1ee844bf0ee469160f5fe430c2
Steven McIntyre <steven.mc2225@gmail.com>
2023-03-17 22:09:28 -0700

    finished second assingment, return a repeated string x number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, mult) {
+  let newstring = '';
+  for (let i = 0; i < mult; i++) {
+    newstring += string;
+  }
+  return newstring;
 };
 
 // Do not edit below this line
