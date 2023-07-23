0f665fda926eb570aaf1d4d321976c774d76e0f2
Steven McIntyre <steven.mc2225@gmail.com>
2023-07-23 09:56:53 -0700

    reset problem to start

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,5 @@
-const repeatString = function(string, mult) {
-  if (mult < 0) return "ERROR"; // from the solution (throw error if neg num)
-  let newstring = '';
-  for (let i = 0; i < mult; i++) {
-    newstring += string;
-  }
-  return newstring;
+const repeatString = function() {
+
 };
 
 // Do not edit below this line
