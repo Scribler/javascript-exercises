7b938ded038cdab7e0f9768d6abaf482f417feea
Steven McIntyre <steven.mc2225@gmail.com>
2023-07-23 16:37:19 -0700

    finished reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let reversedString = '';
+  let reversedArray = [];
+  let stringArray = string.split('');
+  for (letter in stringArray){
+    reversedArray.unshift(stringArray[letter]);
+    reversedString = reversedArray.join('');
+  };
+  // console.log(stringArray);
+  // console.log(reversedString);
 
+  return reversedString;
 };
 
+// reverseString('This is a string, to be split')
 // Do not edit below this line
 module.exports = reverseString;
