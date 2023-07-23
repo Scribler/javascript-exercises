8097dc09276c0f46d1f54fcb9deb0e17a1cada3a
Steven McIntyre <steven.mc2225@gmail.com>
2023-07-23 11:07:33 -0700

    removed unnecessary constants from old build

--- 14_printName/printName.spec.js
+++ 14_printName/printName.spec.js
@@ -1,6 +1,4 @@
 let printName = require('./printName');
-const input1 = 'John';
-const input2 = 'Dallas';
 describe('printName', function(){
   test('Show "My Name"', function(){
     expect(printName('John', 'Dallas')).toEqual('John Dallas');
