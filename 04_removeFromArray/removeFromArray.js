df0d1194f15f7063846a4f6678bc0a670ca3985b
Steven McIntyre <steven.mc2225@gmail.com>
2023-07-26 17:23:47 -0700

    playing with 'rest parameters' and 'spread opperators'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const mainArray = [0, 4, 2, 1, 8, 9, 20, 12, 7];
+const numsToRemove = [1, 3, 4, 5];
 
-};
+
+// const removeFromArray = function(array, ...nums) {
+//   console.log(array)
+// };
+// removeFromArray();
+
+const addNums = function (...toAdd) {
+  let finalNum = 0;
+  for (const num of toAdd) {
+    finalNum += num;
+  }
+  return finalNum;
+}
+
+console.log(addNums(1, 3, 2)); // should print '6'
 
 // Do not edit below this line
-module.exports = removeFromArray;
+// module.exports = removeFromArray;
