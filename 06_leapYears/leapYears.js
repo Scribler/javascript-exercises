2453131de5ec4bfca937854332bb6d3793e8cdfa
Steven McIntyre <steven.mc2225@gmail.com>
2023-07-30 23:34:50 -0700

    texting out ways to calculate leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
 
+const leapYears = function(endYear, testYear) {
+  const leapYearsArray = [];
+  let leapStepper;
+  for (let index = 0; index < endYear; index = index + 4) {
+    // leapStepper += 4;
+    leapYearsArray.push(index);
+  }
+  console.log(leapYearsArray);
+  console.log(`Is ${testYear} a leapyear? : ${leapYearsArray.includes(testYear)}`);
 };
 
+leapYears(400, 200);
 // Do not edit below this line
 module.exports = leapYears;
